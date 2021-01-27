6a94bffb182270c09652c01b7992c6658dc5b7b5
Reuel Bacellar <reubacellar@gmail.com>
2021-01-27 06:13:54 -0300

    First commit

---
+++ clientregister/src/main/java/com/reuel_dev/clientregister/ClientregisterApplication.java
@@ -0,0 +1,13 @@
+package com.reuel_dev.clientregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientregisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientregisterApplication.class, args);
+	}
+
+}
---
+++ clientregister/src/test/java/com/reuel_dev/clientregister/ClientregisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reuel_dev.clientregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientregisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
