af984661add5a314cdd2ca7acef5f6198832fa3c
Reuel Bacellar <reubacellar@gmail.com>
2021-01-29 12:00:06 -0300

    findAll paged

---
+++ clientregister/src/main/java/com/reuel_dev/clientregister/services/ClientService.java
@@ -0,0 +1,25 @@
+package com.reuel_dev.clientregister.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.reuel_dev.clientregister.dto.ClientDTO;
+import com.reuel_dev.clientregister.entities.Client;
+import com.reuel_dev.clientregister.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
+
+}
